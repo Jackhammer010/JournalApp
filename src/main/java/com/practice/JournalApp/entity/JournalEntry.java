3e67b8e8774c9edd8adf09285cd41359c34bfca0
Jackhammer010 <mzakariya737@gmail.com>
2025-09-04 15:41:02 +0530

    Added JournalEntry and User entity classes

---
+++ src/main/java/com/practice/JournalApp/entity/JournalEntry.java
@@ -0,0 +1,15 @@
+package com.practice.JournalApp.entity;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Data
+@Document(collection = "journal_entry")
+public class JournalEntry {
+
+    @Id
+    private ObjectId id;
+    private String title;
+    private String content;
+}
---
+++ src/main/java/com/practice/JournalApp/entity/User.java
@@ -0,0 +1,27 @@
+package com.practice.JournalApp.entity;
+
+import lombok.Data;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+@Data
+@Document(collection = "user")
+public class User {
+
+    @Id
+    private ObjectId id;
+    @NonNull
+    @Indexed(unique = true)
+    private String username;
+    @NonNull
+    private String password;
+    private List<String> roles;
+    @DBRef
+    private List<JournalEntry> journalEntries = new ArrayList<>();
+}
