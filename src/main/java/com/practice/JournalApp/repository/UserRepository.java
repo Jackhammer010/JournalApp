7cade7bb71e61752edcbf411779257f98c69026f
Jackhammer010 <mzakariya737@gmail.com>
2025-09-04 15:44:56 +0530

    Created - User and JournalEntry Repository files

---
+++ src/main/java/com/practice/JournalApp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.practice.JournalApp.repository;
+
+import com.practice.JournalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+}
